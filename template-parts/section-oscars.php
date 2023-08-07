47355f4f7147cd228cbce9bb07ade8c9659c7dfc
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-07 17:27:04 +0200

    Ajout éléments de la section

--- template-parts/section-oscars.php
+++ template-parts/section-oscars.php
@@ -4,3 +4,11 @@ echo ("oscars");
 
 
 ?>
+
+<section>
+    <div class="oscar-section">
+        <p class="oscar-texte">Fleurs d'orangers et chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
+        <img src="./assets/images/oscar-icon.png"></img>
+    </div>
+    
+</section>
