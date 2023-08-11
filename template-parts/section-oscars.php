89b03b12bc768f6a5654c9cab2ac179bb5030c34
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-11 12:17:30 +0200

    MAJ section pour mettre dans le footer

--- template-parts/section-oscars.php
+++ template-parts/section-oscars.php
@@ -1,14 +1,8 @@
-<?php
-
-echo ("oscars");
-
-
-?>
-
 <section>
     <div class="oscar-section">
-        <p class="oscar-texte">Fleurs d'orangers et chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
-        <img src="./assets/images/oscar-icon.png"></img>
+        <h3 class="oscar-texte">Fleurs d'orangers et chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
+        
+        <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/oscar-icon.png';?>"></img>
     </div>
     
 </section>
