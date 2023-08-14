f6d98005856bd26056b2a0de662bf5f1dcd2de80
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-14 20:07:49 +0200

    Modification icone Oscars

--- template-parts/section-oscars.php
+++ template-parts/section-oscars.php
@@ -2,7 +2,7 @@
     <div class="rubrique-oscars">
         <h3 class="texte-oscars">Fleurs d'orangers et chats errants est nominé aux Oscars Short Film Animated de 2023 !</h3>
         
-        <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/icone-oscars.png';?>"></img>
+        <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/icone-oscars2.png';?>"></img>
     </div>
     
 </section>
