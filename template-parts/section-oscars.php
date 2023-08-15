6c204af48d91e44c1ecfeb24d20b5189b1af0610
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-15 19:51:41 +0200

    Modification classes

--- template-parts/section-oscars.php
+++ template-parts/section-oscars.php
@@ -1,8 +1,5 @@
-<section class="oscars">
-    <div class="rubrique-oscars">
-        <h3 class="texte-oscars">Fleurs d'orangers et chats errants est nominé aux Oscars Short Film Animated de 2023 !</h3>
-        
-        <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/icone-oscars2.png';?>"></img>
-    </div>
+<section class="oscars">>
+    <h3 class="texte-oscars">Fleurs d'orangers et chats errants est nominé aux Oscars Short Film Animated de 2023 !</h3>
     
+    <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/icone-oscars2.png';?>"></img>
 </section>
