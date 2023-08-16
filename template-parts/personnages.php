1842135e087603ba345ec3821036437cbbbb222b
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-16 22:12:34 +0200

    Ajout template partiel des personnages

---
+++ template-parts/personnages.php
@@ -0,0 +1,20 @@
+<div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <img src="./assets/images/Kawaneko.png" />
+        </div>
+        <div class="swiper-slide">
+            <img src="./assets/images/Orenjiiro.png" />
+        </div>
+        <div class="swiper-slide">
+            <img src="./assets/images/Pinku.png" />
+        </div>
+        <div class="swiper-slide">
+            <img src="./assets/images/Tenshi.png" />
+        </div>
+        <div class="swiper-slide">
+            <img src="./assets/images/Jaakuna.png" />
+        </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
