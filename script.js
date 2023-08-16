b450b00d0558cd1b6505da51f00029051f7e335a
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-16 22:17:14 +0200

    Ajout Swiper

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-// SwiperJS à mettre en place ici (CDN)
-
 // Vidéo bannière
 
 var video = document.getElementById("background-video");
+
+// Initialize Swiper
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    },
+});
