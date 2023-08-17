9ed31e1e0c331e386954bddea307c0aace1cc0b9
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-17 18:04:38 +0200

    Ajout URL source theme enfant

--- front-page.php
+++ front-page.php
@@ -43,6 +43,7 @@ get_header();
                     // echo '<figcaption>'. $main_character->post_title . '</figcaption>';
                     // echo '</figure>';
                     // $characters_query->next_post();
+                    echo get_stylesheet_directory_uri();
                     get_template_part( 'template-parts/personnages', '' );
                     ?>
                 </div>
