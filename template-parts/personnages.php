ed4f2ded5c9507a4b661abf8db0677e333962350
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-17 19:29:51 +0200

    Ajout du lien URL du thème enfant

--- template-parts/personnages.php
+++ template-parts/personnages.php
@@ -1,19 +1,19 @@
 <div class="swiper mySwiper">
     <div class="swiper-wrapper">
         <div class="swiper-slide">
-            <img src="./assets/images/Kawaneko.png" />
+            <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Kawaneko.png' ?>" />
         </div>
         <div class="swiper-slide">
-            <img src="./assets/images/Orenjiiro.png" />
+            <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Orenjiiro.png' ?>" />
         </div>
         <div class="swiper-slide">
-            <img src="./assets/images/Pinku.png" />
+            <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Pinku.png' ?>" />
         </div>
         <div class="swiper-slide">
-            <img src="./assets/images/Tenshi.png" />
+            <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Tenshi.png'?>" />
         </div>
         <div class="swiper-slide">
-            <img src="./assets/images/Jaakuna.png" />
+            <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/Jaakuna.png' ?>" />
         </div>
     </div>
     <div class="swiper-pagination"></div>
