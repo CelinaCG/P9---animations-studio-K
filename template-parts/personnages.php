d2cfeb4e9a1f94a55991b291fb2e9c2784ee76ee
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-18 11:24:38 +0200

    Ajout script swiper

--- template-parts/personnages.php
+++ template-parts/personnages.php
@@ -17,4 +17,24 @@
         </div>
     </div>
     <div class="swiper-pagination"></div>
-  </div>
+</div>
+
+<!-- Initialize Swiper -->
+<script>
+    var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+</script>
