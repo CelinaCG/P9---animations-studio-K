a44ed37deb6446c68c0e41ccae6268e2cbcfe7ba
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-21 17:58:49 +0200

    Ajout menu burger et suppression de l'ancien menu

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -1,16 +1,31 @@
 <!-- Menu burger -->
-<nav id="site-navigation" class="main-navigation">
-    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-        <span class="line"></span>
-        <span class="line"></span>
-        <span class="line"></span>
-    </button>
+
+<nav class="main-navigation">
     <ul>
-        <li><a href="#story">Histoire</a></li>
-        <li><a href="#characters">Personnages</a></li>
         <li class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
-        <li><a href="#place">Lieu</a></li>
-        <li><a href="#studio">Studio Koukaki</a></li>
+        
+        <div id="mySidenav" class="sidenav">
+        
+        <a id="closeBtn" href="#" class="close">x</a>
+    
+        <ul>
+            <li><a href="#story">Histoire</a></li>
+            <li><a href="#characters">Personnages</a></li>
+            <li class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
+            <li><a href="#place">Lieu</a></li>
+            <li><a href="#studio">Studio Koukaki</a></li>
+        </ul>
+
+        </div>
+        
+        <a href="#" id="openBtn">
+        <span class="burger-icon" >
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+        </span>
+        </a>
     </ul>
 
 </nav>
+
