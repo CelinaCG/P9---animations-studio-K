cac957bee58c113c1091a6be0cf77cc56a3eeff0
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-23 14:55:30 +0200

    Modif script pour test EventListener

--- script.js
+++ script.js
@@ -8,18 +8,24 @@ var sidenav = document.getElementById("mySidenav");
 var openBtn = document.getElementById("openBtn");
 var closeBtn = document.getElementById("closeBtn");
 
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
+let logo = document.querySelector('.petit-logo');
+// openBtn.onclick = openNav;
+// closeBtn.onclick = closeNav;
+
+logo.addEventListener("click", function() {
+  console.log("Test");
+
+});
 
 /* Largeur 1440px */
-function openNav() {
-  sidenav.classList.add("active");
-}
+// function openNav() {
+//   sidenav.classList.add("active");
+// }
 
 /* Set the width of the side navigation to 0 */
-function closeNav() {
-  sidenav.classList.remove("active");
-}
+// function closeNav() {
+//   sidenav.classList.remove("active");
+// }
 
 
 
