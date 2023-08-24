5e98db9b9c57b2e6c11a896c1dc53bd48cc196ea
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-24 12:36:28 +0200

    Suppression crochet en trop

--- template-parts/section-oscars.php
+++ template-parts/section-oscars.php
@@ -1,4 +1,4 @@
-<section class="oscars">>
+<section class="oscars">
     <h3 class="texte-oscars">Fleurs d'orangers et chats errants est nominé aux Oscars Short Film Animated de 2023 !</h3>
     
     <img src="<?php echo get_stylesheet_directory_uri().'/assets/images/icone-oscars2.png';?>"></img>
