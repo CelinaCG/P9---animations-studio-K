705078e438c33665b582cac94bcfb7b639081f45
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-24 12:35:47 +0200

    Suppression div

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -1,23 +1,21 @@
 <!-- Menu burger -->
 
-<nav class="main-navigation burger">
+<nav id="mySidenav" class="sidenav burger">
+ 
     <ul>
-        <div id="mySidenav" class="sidenav">
-
-            <ul>
-                <li class="petit-logo"><img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/petit_logo.png'; ?> " alt="bannière"></li>
-                <li class="histoire burger-hover"><a href="#story">Histoire</a></li>
-                <li class="burger-hover"><a href="#characters">Personnages</a></li>
-                <li class="lieu burger-hover"><a href="#place">Lieu</a></li>
-                <li class="studio burger-hover"><a href="#studio">Studio Koukaki</a></li>
-                
-
-            </ul>
-            <a href="#">STUDIO KOUKAKI</a>
-        </div>
+        <li class="petit-logo"><img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/petit_logo.png'; ?> " alt="bannière"></li>
+        <li class="histoire burger-hover"><a href="#story">Histoire</a></li>
+        <li class="burger-hover"><a href="#characters">Personnages</a></li>
+        <li class="lieu burger-hover"><a href="#place">Lieu</a></li>
+        <li class="studio burger-hover"><a href="#studio">Studio Koukaki</a></li>
         
-        
+
     </ul>
+    <span class="koukaki-menu" href="#">STUDIO KOUKAKI</span>
+
+        
+        
+
     
 </nav>
 
