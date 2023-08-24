e44c73e60d54dd2b786aa8373259197f814c992c
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-24 18:01:07 +0200

    Tests d'eventlistener pour menu burger

--- script.js
+++ script.js
@@ -5,16 +5,34 @@ var video = document.getElementById("background-video");
 // Menu burger
 
 var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
+// var openBtn = document.getElementById("openBtn");
+// var closeBtn = document.getElementById("closeBtn");
+var openBtn = document.getElementsByClassName("menu_burger");
+var closeBtn = document.getElementsByClassName("croix_burger");
+
 
 let logo = document.querySelector('.petit-logo');
 // openBtn.onclick = openNav;
 // closeBtn.onclick = closeNav;
 
-logo.addEventListener("click", function() {
-  console.log("Test");
+// logo.addEventListener("click", function() {
+//   console.log("Test");
 
+// });
+
+// Ouverture menu burger
+openBtn.addEventListener("click", function(){
+  document.getElementById("site-navigation").classList.toggle("croix_burger");
+  // Si click sur la croix, faire disparaitre le menu
+  document.getElementById("site-navigation").classList.remove("menu_burger");
+
+});
+
+// Fermeture menu burger
+closeBtn.addEventListener("click", function(){
+  document.getElementById("site-navigation").classList.toggle("croix_burger");
+  // Si click sur la croix, faire apparaitre le menu burger
+  document.getElementById("site-navigation").classList.remove("menu_burger");
 });
 
 /* Largeur 1440px */
@@ -26,6 +44,7 @@ logo.addEventListener("click", function() {
 // function closeNav() {
 //   sidenav.classList.remove("active");
 // }
+
 
 
 
