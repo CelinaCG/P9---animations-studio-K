285e302195eb5ab39e94bfb2845cf50160f5934b
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-08-28 18:13:46 +0200

    Ajout classe pour animation des titres H2 en scroll

--- front-page.php
+++ front-page.php
@@ -21,7 +21,7 @@ get_header();
         
         <section id="#story" class="story">
             <!-- Classe pour séparer le titre et l'arrière-plan -->
-            <h2><div class="animation-titre">L'histoire</div></h2>
+            <h2 class="titreH2"><div class="animation-titre ">L'histoire</div></h2>
             
             <article id="" class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
@@ -63,7 +63,7 @@ get_header();
         <section id="studio">
             <!-- Classe pour séparer le titre et l'arrière-plan -->
             
-            <h2><div class="animation-titre">Studio Koukaki</div></h2>
+            <h2 class="titreH2"><div class="animation-titre">Studio Koukaki</div></h2>
             
             <div>
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
