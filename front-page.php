8ca1f31c3090492b1a9e48d9a7488f487906f326
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-01 11:03:55 +0200

    MAJ scroll du logo et vidéo

--- front-page.php
+++ front-page.php
@@ -8,24 +8,21 @@ get_header();
         <section class="banner">
             <!-- Parallaxe de la vidéo -->
             <!-- 806 px de hauteur sur 1440 px de largeur -->
-            <div style='position: relative; width: 100%; height: 500px; overflow:hidden;'>
             <video id="background-video" autoplay loop muted>
-            <source src="<?php echo get_stylesheet_directory_uri() . '/assets/videos/video-banniere.mp4'; ?>" type="video/mp4" style='position:absolute;' data-bottom-top='transform:translate3d(0, 0px, 0)' data-top-bottom='transform:translate3d(0, -306px, 0)'>
+            <source src="<?php echo get_stylesheet_directory_uri() . '/assets/videos/video-banniere.mp4'; ?>" type="video/mp4">
             <!-- Appel du thème enfant via get_template_directory_uri -->
             <img src="<?php echo get_template_directory_uri() . '/assets/images/banner.png'; ?> " alt="bannière">
             </video>
-            </div>
-
-            <img class="logo" src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
+            <!-- Position du logo à 300px par rapport au haut de page. -->
+            <!-- Déplacement vers le bas ou le haut de 150px via le scroll à partir de la position initiale du logo -->
+            <img class="logo" data-0="top:300px;" data-150="top:460px;" src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
         </section>
        
         
         <section id="#story" class="story">
             <!-- Classe pour séparer le titre et l'arrière-plan -->
-        
-                <h2 class="titre-wrapper"><div class="section-title">L'histoire</div></h2>
-        
-            
+            <h2 class="titre-wrapper"><div class="section-title">L'histoire</div></h2>
+
             <article id="" class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
             </article>
@@ -60,21 +57,17 @@ get_header();
                         <img class="big_cloud" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/big_cloud.png'; ?> " alt="image grand nuage">
                     </div>
                 </div>
-               
-                
-
             </article>
         </section>
 
 
         <section id="studio">
             <!-- Classe pour séparer le titre et l'arrière-plan -->
-            
-                <h2 class="studio-wrapper"><div class="studio-title">Studio Koukaki</div></h2>
+            <h2 class="studio-wrapper"><div class="studio-title">Studio Koukaki</div></h2>
  
             <div>
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
-                <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
+                <p>Avec une créativité et une capacité d’innovation mondialement reconnue, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
             </div>
             </section>
     </main><!-- #main -->
