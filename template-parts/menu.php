660ba5c261d28d8687345ce3de9066ecf40bc3c8
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-01 15:26:36 +0200

    Ajout classe pour rubrique personnage

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -5,7 +5,7 @@
     <ul>
         <li class="petit-logo"><img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/petit_logo.png'; ?> " alt="bannière"></li>
         <li class="histoire burger-hover"><a href="#story">Histoire</a></li>
-        <li class="burger-hover"><a href="#characters">Personnages</a></li>
+        <li class="perso burger-hover"><a href="#characters">Personnages</a></li>
         <li class="lieu burger-hover"><a href="#place">Lieu</a></li>
         <li class="studio burger-hover"><a href="#studio">Studio Koukaki</a></li>
         
