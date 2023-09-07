69474c23afc051db01743be23dba3e26919c5631
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-07 11:00:19 +0200

    Modif SwiperJS

--- template-parts/personnages.php
+++ template-parts/personnages.php
@@ -25,16 +25,13 @@
       effect: "coverflow",
       grabCursor: true,
       centeredSlides: true,
-      slidesPerView: "auto",
+      slidesPerView: 3,
       coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
+        rotate: 0,
+        stretch: -90,
+        depth: 0,
         modifier: 1,
-        slideShadows: true,
-      },
-      pagination: {
-        el: ".swiper-pagination",
+        slideShadows: false,
       },
     });
 </script>
